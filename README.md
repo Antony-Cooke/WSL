05c5916b0b89405578fde86d131754ae3db11699
Antony-Cooke <38263984+Antony-Cooke@users.noreply.github.com>
2018-06-19 18:29:36 +0100

    Initial Load

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# WSL
+# Wirral_Snooker_League
 Wirral Snooker &amp; Billiards League
+
+This Site is Tony Cooke's attempt to rewrite the original Website which may be found at 
+http://birkenheadsnooker.my-free.website
---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    // jQuery methods go here...
+    $("#btn1").click(function(){
+        $("#UU").toggle();
+    });
+    $("#btn2").click(function(){
+        $("#RB").toggle();
+    });
+ 
+ });
